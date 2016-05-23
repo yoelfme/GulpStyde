d25c46357f8eb68cb241982a86fdf1fbbe69ff38
Yoel Monzon <yoelfme@hotmail.com>
2016-05-23 00:55:47 -0600

    [add] task for watch files .scss

--- lesson1/Gulpfile.js
+++ lesson1/Gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  watch = require('gulp-watch');
 
 var config = {
-    scssDir: './assets/scss',
-    cssDir: './assets/css'
+  scssDir: './assets/scss',
+  cssDir: './assets/css'
 };
 
 gulp.task('style', function () {
-    gulp.src(config.scssDir + '/*.scss')
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest(config.cssDir))
+  gulp.src(config.scssDir + '/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest(config.cssDir))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.scssDir + '/**/*.scss', ['style']);
 });
